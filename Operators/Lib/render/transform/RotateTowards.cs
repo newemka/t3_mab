3e887860d6e0b328f8c3be10f7e439e1f47fcd1d
newemka <newemka@gmail.com>
2025-06-18 18:12:02 +0200

    RotateTowards test01

--- Operators/Lib/render/transform/RotateTowards.cs
+++ Operators/Lib/render/transform/RotateTowards.cs
@@ -8,7 +8,7 @@ internal sealed class RotateTowards : Instance<RotateTowards>
 {
     [Output(Guid = "772609fe-ec92-43a9-b8c6-9055bbf0310b")]
     public readonly Slot<Command> Output = new();
-        
+
     public RotateTowards()
     {
         Output.UpdateAction += Update;
@@ -18,11 +18,10 @@ internal sealed class RotateTowards : Instance<RotateTowards>
     {
         var targetMode = LookTowards.GetEnumValue<Modes>(context);
         var targetPos = AlternativeTarget.GetValue(context);
-            
+
         Vector3 targetPosDx;
         if (targetMode == Modes.TowardsCamera)
         {
-            //TransformCallback?.Invoke(this, context); // this this is stupid stupid
             Matrix4x4.Invert(context.WorldToCamera, out var camToWorld);
             targetPosDx = Vector4.Transform(new Vector4(0f, 0f, 0f, 1f), camToWorld).ToVector3();
         }
@@ -30,21 +29,42 @@ internal sealed class RotateTowards : Instance<RotateTowards>
         {
             targetPosDx = targetPos;
         }
-            
-        var sourcePos = Vector4.Transform( new Vector4(0,0,0,1), context.ObjectToWorld).ToVector3();
-            
-        var lookAt = GraphicsMath.LookAtRH(Vector3.Zero , -targetPosDx + sourcePos, VectorT3.Up);
+
+        var sourcePos = Vector4.Transform(new Vector4(0, 0, 0, 1), context.ObjectToWorld).ToVector3();
+        var direction = targetPosDx - sourcePos;
+
+        Matrix4x4 lookAt;
+        switch (targetMode)
+        {
+            case Modes.TowardPositionLockedXZ:
+                // Lock X and Z rotation (only Y rotation)
+                direction.Y = 0;
+                direction = Vector3.Normalize(direction);
+                lookAt = GraphicsMath.LookAtRH(Vector3.Zero, -direction, new Vector3(0f, 1f, 0f));
+                break;
+
+            case Modes.TowardPositionLockedZ:
+                // Lock Z rotation (allow X and Y rotation)
+                var flatForward = Vector3.Normalize(new Vector3(direction.X, direction.Y, 0));
+                lookAt = GraphicsMath.LookAtRH(Vector3.Zero, -direction, new Vector3(0f, 1f, 0f));
+                break;
+
+            default:
+                // Original behavior for other modes
+                lookAt = GraphicsMath.LookAtRH(Vector3.Zero, -direction, new Vector3(0f, 1f, 0f));
+                break;
+        }
+
         Matrix4x4.Invert(lookAt, out lookAt);
 
         var rotationOffset = RotationOffset.GetValue(context);
         var rotateOffset = Matrix4x4.CreateFromYawPitchRoll(
-                                                            rotationOffset.Y.ToRadians(),
-                                                            rotationOffset.X.ToRadians(),
-                                                            rotationOffset.Z.ToRadians());
+            rotationOffset.Y.ToRadians(),
+            rotationOffset.X.ToRadians(),
+            rotationOffset.Z.ToRadians());
 
-        lookAt = Matrix4x4.Multiply( rotateOffset, lookAt);
-            
-            
+        lookAt = Matrix4x4.Multiply(rotateOffset, lookAt);
+
         var previousWorldTobject = context.ObjectToWorld;
         context.ObjectToWorld = Matrix4x4.Multiply(lookAt, context.ObjectToWorld);
         Command.GetValue(context);
@@ -53,14 +73,13 @@ internal sealed class RotateTowards : Instance<RotateTowards>
 
     [Input(Guid = "ae8b45e6-e72f-40fe-8ff9-34cd9fffc164")]
     public readonly InputSlot<Command> Command = new();
-        
+
     [Input(Guid = "1ce44784-db0d-42ab-b00b-09a1ef2e9042")]
     public readonly InputSlot<Vector3> AlternativeTarget = new();
-        
+
     [Input(Guid = "a000747f-578f-4367-bc47-e8bb56252043")]
     public readonly InputSlot<Vector3> RotationOffset = new();
-        
-        
+
     [Input(Guid = "CCD2CC62-AD7B-420A-95D4-243257291619", MappedType = typeof(Modes))]
     public readonly InputSlot<int> LookTowards = new();
 
@@ -68,5 +87,7 @@ internal sealed class RotateTowards : Instance<RotateTowards>
     {
         TowardsCamera,
         TowardsPosition,
+        TowardPositionLockedXZ,
+        TowardPositionLockedZ
     }
 }
