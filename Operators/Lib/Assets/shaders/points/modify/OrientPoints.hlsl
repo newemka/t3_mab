fe2d0934cd031a418e414eb21642dc7109a99ebd
newemka <newemka@gmail.com>
2026-03-28 22:38:39 +0100

    Update:[OrientPoints] orient to move direction

--- Operators/Lib/Assets/shaders/points/modify/OrientPoints.hlsl
+++ Operators/Lib/Assets/shaders/points/modify/OrientPoints.hlsl
@@ -33,6 +33,7 @@ cbuffer Transforms : register(b2)
 };
 
 StructuredBuffer<Point> SourcePoints : t0;
+StructuredBuffer<Point> PreviousSourcePoints : t1;
 RWStructuredBuffer<Point> ResultPoints : u0;
 
 [numthreads(64, 1, 1)] void main(uint3 i : SV_DispatchThreadID)
@@ -55,14 +56,15 @@ RWStructuredBuffer<Point> ResultPoints : u0;
 
     float sign = Flip > 0.5 ? -1 : 1;
     
-
+    
      switch (OrientationMode) {
         case 0: // Center
-            float4 newRot = qLookAt(normalize(Target - p.Position) * sign, normalize(UpVector));
-            float3 forward = qRotateVec3(float3(0, 0, 1), newRot);
-            float4 alignment = qFromAngleAxis(3.141578, forward);
+            {float4 newRot = qLookAt(normalize(Target - p.Position) * sign, normalize(UpVector));
+             float3 forward = qRotateVec3(float3(0, 0, 1), newRot);
+             float4 alignment = qFromAngleAxis(PI, forward);
              newRot = qMul(alignment, newRot);
              p.Rotation = normalize(qSlerp(normalize(p.Rotation), normalize(newRot), strength));
+            }
         break;
            
         case 1: // Screen
@@ -73,6 +75,33 @@ RWStructuredBuffer<Point> ResultPoints : u0;
             float3 up=mul(float3(0,-1,0),(float3x3)CameraToWorld);
             float3 dir=normalize(p.Position-CameraToWorld[3].xyz);
             p.Rotation = normalize(qSlerp(p.Rotation, qLookAt(-dir,up), strength));
+        break;
+
+        case 3: // Moving direction
+        {
+
+            Point pp = PreviousSourcePoints[i.x];
+            float3 moveDir = p.Position - pp.Position;
+            
+           
+            float3 upVector = UpVector;
+            moveDir = normalize(moveDir) * sign;
+            if (abs(dot(normalize(UpVector),normalize(moveDir))) > .8){
+                upVector = float3(1,0,0);
+            }  
+            float4 newRot = qLookAt(moveDir, normalize(upVector));
+            
+            // Adjust orientation so Z+ points forward
+            float3 forward = qRotateVec3(float3(0, 0, 1), newRot);
+            float4 alignment = qFromAngleAxis(PI, forward); // Use PI constant
+            newRot = qMul(alignment, newRot);
+            strength *= smoothstep(0.0,0.001,length(moveDir));
+            p.Rotation = normalize(qSlerp(normalize(ResultPoints[i.x].Rotation), normalize(newRot),  saturate(strength)));
+           if (length(moveDir)<.01){
+            p.Rotation = ResultPoints[i.x].Rotation;
+           }
+           // p.Rotation = ResultPoints[i.x].Rotation;
+        }
         break;
         
     }
